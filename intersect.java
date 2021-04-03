50269edabc0270a92eb9d93c18336a6a947db1fa
kyleyiyang <kyleyiyang@gmail.com>
2021-04-02 22:06:39 -0400

    Update intersect.java

--- intersect.java
+++ intersect.java
@@ -19,3 +19,26 @@ class Solution {
         return arr;
     }
 }
+
+***********************************************************************
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer,Integer> map=new HashMap<>();
+        List<Integer> lst = new ArrayList<>();
+        for (int i:nums1) {
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for (int j:nums2) {
+            if (map.containsKey((Integer)j)&& map.get((Integer)j)>0) {
+                map.put((Integer)j,map.getOrDefault((Integer)j,0)-1);
+                lst.add(j);
+            }
+        }
+        int[] ans=new int[lst.size()];
+        for (int m=0;m<ans.length;m++) {
+            ans[m]=lst.get(m).intValue();
+        }
+        return ans;
+    }
+}
